d5bcb0024ccb99862923e6decdde73a138119767
Thomas Titscher <thomas.titscher@gmail.com>
2021-01-19 14:39:40 +0100

    Remove matplotlib from test file

--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -1,8 +1,6 @@
 import unittest
 import numpy as np
-import matplotlib.pyplot as plt
 import bayes
-
 
 
 class TestCorrelation(unittest.TestCase):
@@ -15,21 +13,21 @@ class TestCorrelation(unittest.TestCase):
         trans = bayes.correlation.transformation_from_correlation(corr)
         self.assertEqual(trans.shape[0], 10)
         self.assertLess(trans.shape[1], 5)
-    
+
     def test_uncorrelated(self):
         """corr should be an identity matrix"""
         x = np.linspace(0, 1, 10)
         corr = bayes.correlation.correlation(x, 0.02)
         self.assertLess(np.linalg.norm(corr - np.eye(10)), 1e-6)
-        
+
         trans = bayes.correlation.transformation_from_correlation(corr)
         self.assertEqual(trans.shape[0], 10)
         self.assertEqual(trans.shape[1], 10)
-    
+
     def test_invalid_2d(self):
         """corr should be an identity matrix"""
         x = np.eye(10)
-        self.assertRaises(Exception, bayes.correlation.correlation, x, 1.)
+        self.assertRaises(Exception, bayes.correlation.correlation, x, 1.0)
 
     def test_with_MNV(self):
         np.random.seed(6174)
@@ -39,30 +37,28 @@ class TestCorrelation(unittest.TestCase):
         values = np.random.multivariate_normal(np.zeros(len(x)), corr)
         trans = bayes.correlation.transformation_from_correlation(corr)
         # ???
-       
+
+
 class TestCorrelatedVB(unittest.TestCase):
     def run_vb(self, n_x, L_data, L_model):
         np.random.seed(6174)
 
-
         param_true = (7.0, 10.0)
         noise_std = 0.2
         x = np.linspace(0, 1, n_x)
-
 
         def fw(parameters):
             m, c = parameters
             return c + x * m
 
         perfect_data = fw(param_true)
-        corr = bayes.correlation.correlation(x, L_data)  
+        corr = bayes.correlation.correlation(x, L_data)
         correlated_noise = np.random.multivariate_normal(
             np.zeros(len(x)), corr * noise_std ** 2, 1
         )[0]
 
         data = perfect_data + correlated_noise
-        
-        
+
         transformation = bayes.correlation.transformation(x, L_model)
 
         def model_error(prm):
@@ -80,8 +76,8 @@ class TestCorrelatedVB(unittest.TestCase):
         sd50 = self.run_vb(50, L_data=0.5, L_model=1e-6).std_diag[0]
         sd200 = self.run_vb(200, L_data=0.5, L_model=1e-6).std_diag[0]
 
-        self.assertLess(sd200, sd50/2.)
-    
+        self.assertLess(sd200, sd50 / 2.0)
+
     def test_demonstrate_solution(self):
         """
         Including the correlation in the model 
@@ -91,7 +87,8 @@ class TestCorrelatedVB(unittest.TestCase):
         sd50 = self.run_vb(50, L_data=0.5, L_model=0.4).std_diag[0]
         sd200 = self.run_vb(200, L_data=0.5, L_model=0.4).std_diag[0]
 
-        self.assertAlmostEqual(sd200, sd50, delta=sd50/100)
+        self.assertAlmostEqual(sd200, sd50, delta=sd50 / 100)
+
 
 if __name__ == "__main__":
     unittest.main()
