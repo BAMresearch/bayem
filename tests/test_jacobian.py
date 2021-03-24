b2d5b1509dbab2a1548c7c4eed211e473167d72e
Thomas Titscher <thomas.titscher@bam.de>
2021-03-24 11:07:44 +0100

    Add test case to show partial definition of Jac

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -15,6 +15,28 @@ class DummyME(ModelErrorInterface):
         return {"out1": self.xs * A + B ** 2, "out2": self.xs * A ** 2 + B * self.xs}
 
 
+class DummyMEPartial(ModelErrorInterface):
+    def __init__(self):
+        super().__init__()
+        self.parameter_list.define("A", 42.0)
+        self.parameter_list.define("B", 0.0)
+        self.xs = np.linspace(0.0, 1.0, 3)
+
+    def __call__(self):
+        A, B = self.parameter_list["A"], self.parameter_list["B"]
+        return {"out1": self.xs * A + B ** 2, "out2": self.xs * A ** 2 + B * self.xs}
+
+    def jacobian(self):
+        """
+        We can provide the derivative w.r.t. "A" analytically and use the
+        central differences of the superclass for the parameter "B".
+        """
+        jac = super().jacobian(["B"])
+        jac["out1"]["A"] = self.xs
+        jac["out2"]["A"] = 2 * self.parameter_list["A"] * self.xs
+        return jac
+
+
 class DummyMEVectorPrm(ModelErrorInterface):
     def __init__(self):
         super().__init__()
@@ -59,6 +81,16 @@ class TestJacobian(unittest.TestCase):
 
         jac_correct = np.array([[1, 1]])
         np.testing.assert_array_almost_equal(jac["out"]["X"], jac_correct)
+
+    def test_partial_jacobian_definition(self):
+        me = DummyMEPartial()
+        A, B = me.parameter_list["A"], me.parameter_list["B"]
+        jac = me.jacobian()
+        check = np.testing.assert_array_almost_equal  # just to make it shorter
+        check(jac["out1"]["A"], me.xs)
+        check(jac["out1"]["B"], 2 * B * np.ones_like(me.xs))
+        check(jac["out2"]["A"], me.xs * 2 * A)
+        check(jac["out2"]["B"], me.xs)
 
 
 if __name__ == "__main__":
