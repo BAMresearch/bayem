52645c7a33bac4073f8f4ac5fc1a1d132a4fc11b
Jörg F. Unger <joerg.unger@bam.de>
2021-04-11 12:00:01 +0200

    version via setuptools_scm and move most stuff to static setup.cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-setuptools.setup(
-    name="bayes",
-    version="0.1",
-    install_requires=requirements,
-    author="BAM",
-    author_email="thomas.titscher@bam.de",
-    description="Variational Bayes",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setuptools.setup()
