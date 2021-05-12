15f512c9c96f8c670fafa9eb4db201725c251713
Atul Agrawal <atulagrawal0711@gmail.com>
2021-05-12 13:44:36 +0200

    Added Viz

--- bayes/Caliberation.py
+++ bayes/Caliberation.py
@@ -1,4 +1,6 @@
-
+#---------
+# AUTHOR: Atul Agrawal (atul.agrawal@tum.de)
+#---------
 import numpy as np
 import torch as th
 
@@ -14,16 +16,16 @@ class Inference:
                  obs_noise_parameters):
         """
 
-        :param prior_dist:
-        :param prior_hyperparameters:
+        :param prior_dist: [string] Specify the prior distribution
+        :param prior_hyperparameters: [2x1] Specify the hyperparameters of the prior dist.
         :param forward_solve:
             * A wrapper to the forward solve to be provided here which internally calls the forward solve,
             takes in known input given by fw_input and takes in parameters to be calibrated and returns the model output which acts as a mean.
 
-        :param fw_input:
-        :param observed_data:
-        :param obs_noise_dist:
-        :param obs_noise_parameters:
+        :param fw_input: [] known input to the forward solve
+        :param observed_data [N,] [tensor]: The observed noisy data
+        :param obs_noise_dist: [string] - Specify the noise distribution
+        :param obs_noise_parameters: Hyperaparameters of the noise model
         """
         self.prior_dist = prior_dist
         self.prior_hyperparameters = prior_hyperparameters
@@ -35,21 +37,31 @@ class Inference:
 
     def model(self, observed_data):
         # --prior
+        #--- https://github.com/stan-dev/stan/wiki/Prior-Choice-Recommendations
         #TODO: Incorporate prior_dist user choice here
-        self.para_prior = dist.Normal(self.prior_hyperparameters[0], self.prior_hyperparameters[1])
+        if self.prior_dist == "Normal":
+            self.para_prior = dist.Normal(self.prior_hyperparameters[0], self.prior_hyperparameters[1])
+        if self.prior_dist == "Uniform":
+            self.para_prior = dist.Uniform(self.prior_hyperparameters[0], self.prior_hyperparameters[1])
+        if self.prior_dist == "Gamma":
+            self.para_prior = dist.Gamma(self.prior_hyperparameters[0], self.prior_hyperparameters[1])
+        if self.prior_dist == "Beta":
+            self.para_prior = dist.Beta(self.prior_hyperparameters[0], self.prior_hyperparameters[1])
         para = pyro.sample("theta", self.para_prior)
-
         # --likelihood
         mean = self.forward_solve(self.fw_input, para)
         #TODO: Incorporate noise model dist choice here, default is normal
-        self.likelihood = dist.Normal(mean, self.obs_noise_parameters ** 2)
+        if self.obs_noise_dist == "Normal":
+            self.likelihood = dist.Normal(mean, self.obs_noise_parameters ** 2)
+        else:
+            raise NotImplementedError
         pyro.sample("lkl", self.likelihood, obs=observed_data)
 
         return para
 
     def run(self, n, kernel=None):
-
-        if kernel is None:
+        #TODO: Incorporate VI
+        if kernel == "NUTS":
             kernel = NUTS(self.model)
         if kernel == "HMC":
             print('Using HMC kernel')
@@ -60,6 +72,31 @@ class Inference:
         posterior = mcmc.get_samples()['theta'].numpy()
         # print(posterior)
         mcmc.summary()
+        return posterior
 
-    def visualize():
-        raise NotImplementedError
+    def visualize(self, posterior):
+        """
+
+        :param posterior: Pass the posterior after MCMC
+        :return:
+        """
+
+        plt.figure(figsize=(3, 3))
+        sns.histplot(posterior, kde=True, label="para_posterior", bins=20)
+        plt.legend()
+        plt.xlabel("parameter")
+        plt.ylabel("Samples")
+        plt.show()
+
+        # plotting priors
+        smpl = np.ndarray((100000))
+        for i in range(1, 100000):
+            smpl[i] = (pyro.sample("AA", self.para_prior))
+        plt.figure(figsize=(3, 3))
+        sns.kdeplot(data=smpl, label="para_prior")
+        plt.legend()
+        plt.xlabel("parameter")
+        plt.ylabel("Density")
+        plt.show()
+
+        #raise NotImplementedError
