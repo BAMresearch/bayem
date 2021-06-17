9fdcd103ea7237e5e8c1409b4432d39ae02054ac
Thomas Titscher <thomas.titscher@bam.de>
2021-06-17 12:10:48 +0200

    Fix error in loglike (sigma**4 instead of sigma*2)

--- bayes/noise.py
+++ bayes/noise.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .parameters import ParameterList
+import math
 
 """
 The inference problem provides:
@@ -83,13 +84,20 @@ class UncorrelatedNoiseModel(NoiseModelInterface):
         overwritten
         """
         terms = self.model_error_terms(model_error_dict)
-        sigma = 1.0 / self.parameter_list["precision"] ** 0.5
+        prec = self.parameter_list["precision"]
+        # sigma = 1.0 / self.parameter_list["precision"] ** 0.5
         ll = 0.0
         for error in terms:
-            ll += -0.5 * (
-                len(error) * np.log(2.0 * np.pi * sigma ** 2)
-                + np.sum(np.square(error / sigma ** 2))
-            )
+            N = len(error)
+            ll -= N/2 * math.log(2*math.pi)
+            ll -= N/2 * math.log(1/prec)
+            ll -= 0.5 * prec * np.sum(np.square(error))
+
+
+            # ll += -0.5 * (
+                # len(error) * np.log(2.0 * np.pi / prec)
+                # + np.sum(np.square(error))*prec
+            # )
         return ll
 
     def _by_noise(self, dict_of_dicts):
