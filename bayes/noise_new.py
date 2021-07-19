96ab10a88be273bcf8a6ad40f99305c53e033e0a
aklawonn <aklawonn@WS8469.zit.bam.de>
2021-07-19 09:58:40 +0200

    Added explanations to noise_new.py

--- bayes/noise_new.py
+++ bayes/noise_new.py
@@ -23,14 +23,14 @@ class NoiseTemplate:
         self.prms_def = prms_def
         self.prms_dim = len_or_one(prms_def)
 
-    def loglike_contribution(self, model_error, prms):
+    def loglike_contribution(self, model_error_vector, prms):
         """
         Evaluates the log-likelihood function for the given model error and
         the given noise parameter vector. This method has to be overwritten.
 
         Parameters
         ----------
-        model_error : array_like
+        model_error_vector : array_like
             Usually a 1D numerical array containing the model errors.
         prms : array_like
             The parameter vector of the noise model.
@@ -47,11 +47,20 @@ class NoiseTemplate:
 
 
 class NormalNoise(NoiseTemplate):
-    def loglike_contribution(self, model_error, prms):
+    """
+    A simple Gaussian (normal) noise model without any correlations.
+    """
+    def loglike_contribution(self, model_error_vector, prms):
+        """
+        This method overwrites the corresponding method of the parent class.
+        Checkout the docstring there for additional information.
+        """
+        # the precision 'prec' is defined as the inverse of the variance, hence
+        # prec = 1 / sigma**2 where sigma denotes the standard deviation
         prec = prms[0]
         ll = 0.0
         # evaluate the Gaussian log-PDF with zero mean and a variance of
         # 1/prec for each error term and sum them up
-        ll -= len(model_error) / 2 * math.log(2 * math.pi / prec)
-        ll -= 0.5 * prec * np.sum(np.square(model_error))
+        ll -= len(model_error_vector) / 2 * math.log(2 * math.pi / prec)
+        ll -= 0.5 * prec * np.sum(np.square(model_error_vector))
         return ll
