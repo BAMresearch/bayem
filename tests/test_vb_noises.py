1839d20fd82fcae5455d361f86ce268bef2a0edd
Abbas Jafari <ajafari@ws8092.zit.bam.de>
2021-12-10 16:15:25 +0100

    Add test for update of only some of noises taking effect

--- tests/test_vb_noises.py
+++ tests/test_vb_noises.py
@@ -33,7 +33,7 @@ class ModelError:
 
 
 class TestTwoNoises(unittest.TestCase):
-    def run_test(self, noise_prior, delta):
+    def run_test(self, noise_prior, delta, update_noise=True):
         """
         Infers two parameters of a linear model where the data is arranged
         in two noise groups.
@@ -55,7 +55,7 @@ class TestTwoNoises(unittest.TestCase):
         param_prior = bayem.MVN([6.0, 2.0], [[1 / 15.0 ** 2, 0], [0, 1 / 7.0 ** 2]])
 
         me = ModelError(fw, data)
-        info = bayem.vba(me, param_prior, noise_prior)
+        info = bayem.vba(me, param_prior, noise_prior, update_noise=update_noise)
 
         param = info.param
         self.assertAlmostEqual(param.mean[0], PRM_A, delta=2 * param.std_diag[0])
@@ -68,6 +68,7 @@ class TestTwoNoises(unittest.TestCase):
         print(param)
         print(noise_sds)
         print()
+        return info
 
     def test_proper_noise(self):
         """Use a noise_prior that is based on the actual values"""
@@ -97,6 +98,26 @@ class TestTwoNoises(unittest.TestCase):
         wrong_noise = bayem.Gamma((1, 1), (2, 2))
         with self.assertRaises(Exception):
             bayem.vba(me, bayem.MVN(7, 12), wrong_noise)
+    
+    def test_several_noises_mixed(self):
+        """ Test if an identifiable noise group is updated and another that is prescribed is not."""
+        noise_prior = {}
+        update_noise = {}
+        noise_prior["group0"] = bayem.Gamma.FromSDQuantiles(
+            0.9 * NOISE0_SD, 1.1 * NOISE0_SD
+        )
+        update_noise["group0"] = False
+        noise_prior["group1"] = bayem.Gamma.FromSDQuantiles(
+            0.1 * NOISE1_SD, 10 * NOISE1_SD
+        )
+        update_noise["group1"] = True
+        info = self.run_test(noise_prior, delta=0.05, update_noise=update_noise)
+        
+        assert (info.noise['group0'].scale == noise_prior['group0'].scale)
+        assert (info.noise['group0'].shape == noise_prior['group0'].shape)
+        
+        assert (info.noise['group1'].scale != noise_prior['group1'].scale)
+        assert (info.noise['group1'].shape != noise_prior['group1'].shape)
 
 
 if __name__ == "__main__":
