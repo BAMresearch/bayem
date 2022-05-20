333a764a1165908bc95a0fff9953769548bf21b9
Thomas Titscher <thomas.titscher@gmail.com>
2022-05-20 13:13:06 +0200

    Remove args in vba methods.
    One idea was to pass additional arguments to the method, like it is
    somehow done in scipy:
    
    def f(x, data):
       return 42*x - data
    
    optimize(f, x0=0, args=some_data)
    
    This is better discussed in a separate issue...

--- bayem/vba.py
+++ bayem/vba.py
@@ -13,7 +13,7 @@ from .distributions import MVN, Gamma
 logger = logging.getLogger(__name__)
 
 
-def vba(f, x0, noise0=None, jac=None, args=None, **option_kwargs):
+def vba(f, x0, noise0=None, jac=None, **option_kwargs):
     """
     Implementation of
         Variational Bayesian inference for a nonlinear
